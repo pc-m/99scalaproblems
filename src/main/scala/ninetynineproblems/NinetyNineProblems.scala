2391d24f3c69864655faec6af0ad17faf6b18110
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-04-12 22:52:52 -0400

    Add problem 2 to 7

--- src/main/scala/ninetynineproblems/NinetyNineProblems.scala
+++ src/main/scala/ninetynineproblems/NinetyNineProblems.scala
@@ -1,7 +1,33 @@
 package ninetynineproblems
 
+import scala.annotation.tailrec
+
 object NinetyNineProblems {
 
-  def last(l: List[Int]) = l last
+  def last(l: List[Int]) = l.last
 
+  def penultimate(l: List[Int]) = l.takeRight(2) head
+
+  def nth(n: Int, l: List[Int]) = l(n)
+
+  def length(l: List[Int]) = l length
+
+  def reverse(l: List[Int]) = l reverse
+
+  @tailrec
+  def isPalindrome(l: List[Int]): Boolean = {
+    if (l.length <= 1) true
+    else if (l.head != l.last) false
+    else isPalindrome(l.tail.take(l.length - 2))
+  }
+
+  def flatten(l: List[Any]): List[Int] = {
+    def iter(l: List[Any], acc: List[Int]): List[Int] = l match {
+      case Nil => acc
+      case (i: Int) :: tail => iter(tail, acc :+ i)
+      case (xs: List[Any]) :: tail => iter(tail, iter(xs, acc))
+      case _ => acc
+    }
+    iter(l, List[Int]())
+  }
 }
--- src/test/scala/ninetynineproblems/NinetyNineProblemsSuite.scala
+++ src/test/scala/ninetynineproblems/NinetyNineProblemsSuite.scala
@@ -12,4 +12,30 @@ class NinetyNineProblemsSuite extends FunSuite {
   test("Problem 1: last") {
     assert(last(List(1, 1, 2, 3, 5, 8)) === 8)
   }
+
+  test("Problem 2: Last but one") {
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) === 5)
+  }
+
+  test("Problem 3: nth element of a list") {
+    assert(nth(2, List(1, 1, 2, 3, 5, 8)) === 2)
+  }
+
+  test("Problem 4: Number of elements of a list") {
+    assert(length(List(1, 1, 2, 3, 5, 8)) === 6)
+  }
+
+  test("Problem 5: Reverse of a list") {
+    assert(reverse(List(1, 1, 2, 3, 5, 8)) === List(8, 5, 3, 2, 1, 1))
+  }
+
+  test("Problem 6: Check if a list is a palindrome") {
+    assert(isPalindrome(List(1, 2, 3, 2, 1)) === true)
+    assert(isPalindrome(List(1, 2, 2, 1)) === true)
+    assert(isPalindrome(List(1, 2, 2)) === false)
+  }
+
+  test("Problem 7: Flatten a nested list") {
+    assert(flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8))
+  }
 }
